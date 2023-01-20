7b9804600259151426be3af81b354f8f880cde17
Ahmed2000501 <91163313+Ahmed2000501@users.noreply.github.com>
2023-01-20 02:00:20 +0200

    Add files via upload

---
+++ js/mien.js
@@ -0,0 +1,10 @@
+let img =document.querySelector('.img');
+let contenr =document.querySelector('.contenr');
+
+function phones(phone){
+    img.src =phone;;
+}
+
+function colors(color){
+    contenr.style.background =color;
+}
